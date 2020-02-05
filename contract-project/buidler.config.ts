c7a089136a0a57acf440ec67e62bedd3e1123f1b
Paul Cowgill <pauldavidcowgill@gmail.com>
2020-02-04 20:11:00 -0600

    Deployed to testnet

--- contract-project/buidler.config.ts
+++ contract-project/buidler.config.ts
@@ -1,35 +1,46 @@
-import { BuidlerConfig, usePlugin } from "@nomiclabs/buidler/config";
+import {BuidlerConfig, usePlugin} from "@nomiclabs/buidler/config";
+
+require("dotenv-flow").config();
 
 usePlugin("@nomiclabs/buidler-waffle");
 usePlugin("@nomiclabs/buidler-solhint");
+usePlugin("@nomiclabs/buidler-etherscan");
 
-// Note: Don't actually put these values here. Get it from an
-// env file that isn't in version control
-const INFURA_API_KEY = "";
-const RINKEBY_PRIVATE_KEY = "";
-const ETHERSCAN_API_KEY = "";
+const INFURA_API_KEY = process.env.INFURA_API_KEY;
+const RINKEBY_PRIVATE_KEY = process.env.RINKEBY_PRIVATE_KEY;
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY;
+
+let accounts = [""];
+if (RINKEBY_PRIVATE_KEY) {
+    accounts = [RINKEBY_PRIVATE_KEY];
+}
+
+let etherscanAPIKeyToUse = "";
+if (ETHERSCAN_API_KEY) {
+    etherscanAPIKeyToUse = ETHERSCAN_API_KEY;
+}
 
 const config: BuidlerConfig = {
-  solc: {
-    version: "0.5.15"
-  },
-  paths: {
-    artifacts: "./artifacts"
-  },
-  defaultNetwork: "buidlerevm",
-  networks: {
-    rinkeby: {
-      url: `https://rinkeby.infura.io/v3/${INFURA_API_KEY}`,
-      accounts: [RINKEBY_PRIVATE_KEY]
+    solc: {
+        version: "0.5.15"
+    },
+    paths: {
+        artifacts: "./artifacts"
+    },
+    defaultNetwork: "buidlerevm",
+    networks: {
+        rinkeby: {
+            url: `https://rinkeby.infura.io/v3/${INFURA_API_KEY}`,
+            accounts: accounts
+        }
+    },
+    etherscan: {
+        // The url for the Etherscan API you want to use.
+        url: "https://api-rinkeby.etherscan.io/api",
+        // Your API key for Etherscan
+        // Obtain one at https://etherscan.io/
+        apiKey: etherscanAPIKeyToUse
     }
-  },
-  etherscan: {
-    // The url for the Etherscan API you want to use.
-    url: "https://api-rinkeby.etherscan.io/api",
-    // Your API key for Etherscan
-    // Obtain one at https://etherscan.io/
-    apiKey: ETHERSCAN_API_KEY
-  }
 };
 
 export default config;
