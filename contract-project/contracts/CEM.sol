e9c8f9e36b9fc70197612ff20c19bdd63d7de047
Paul Cowgill <pauldavidcowgill@gmail.com>
2020-02-07 17:57:15 -0600

    Made single val getters, token transfers almost working

--- contract-project/contracts/CEM.sol
+++ contract-project/contracts/CEM.sol
@@ -4,6 +4,8 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20Detailed.sol";
 
 
+// Note: Probably won't need to create this token
+// as long as the funds from EF arrive in time
 contract CEM is ERC20, ERC20Detailed {
     constructor(uint256 initialSupply)
         public
--- contract-project/contracts/Contributions.sol
+++ contract-project/contracts/Contributions.sol
@@ -2,12 +2,21 @@ pragma solidity ^0.5.15;
 // needed to use structs everywhere before Solidity 0.6.x
 // pragma experimental ABIEncoderV2;
 
-import "@openzeppelin/contracts/ownership/Ownable.sol";
 import "@nomiclabs/buidler/console.sol";
-import "./CEM.sol";
+
+import "@openzeppelin/contracts/ownership/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/SafeERC20.sol";
+import "@openzeppelin/contracts/math/SafeMath.sol";
 
 
 contract Contributions is Ownable {
+    using SafeMath for uint256;
+    using SafeERC20 for IERC20;
+
+    // ERC20 basic token contract being held
+    IERC20 private _token;
+
     mapping(address => address[]) private _senderAddresses;
     mapping(address => uint256[]) private _contributionAmounts;
 
@@ -20,6 +29,17 @@ contract Contributions is Ownable {
     //   address recipient;
     //   uint256 amount;
     // }
+
+    function setToken(IERC20 token) public onlyOwner {
+        _token = token;
+    }
+
+    /**
+     * @return the token used for this contract
+     */
+    function token() public view returns (IERC20) {
+        return _token;
+    }
 
     function getRecipientAddress(string memory nickname)
         public
@@ -43,6 +63,9 @@ contract Contributions is Ownable {
         delete _nicknames[nickname];
     }
 
+    // TODO: Set up a way for a user to approve transfers of the
+    // tokens using this contract
+
     function contribute(address sender, string memory nickname, uint256 amount)
         public
         returns (bool)
@@ -57,8 +80,45 @@ contract Contributions is Ownable {
         _senderAddresses[recipient].push(sender);
         _contributionAmounts[recipient].push(amount);
         // Transfer the token as an internal tx if ERC20 approved
+        _deliverTokens(sender, recipient, amount);
         return true;
     }
+
+    /**
+     * @dev Gets the total count of individual contributions stored by the
+     * contract for a given recipient
+     * @return uint256 representing the total amount of tokens
+     */
+    function getContributionCount(string memory nickname)
+        public
+        view
+        returns (uint256)
+    {
+        address recipient = getRecipientAddress(nickname);
+        return _contributionAmounts[recipient].length;
+    }
+
+    // Single-value getters
+
+    function getContributorAtIndex(string memory nickname, uint256 index)
+        public
+        view
+        returns (address senderAddress)
+    {
+        address[] memory senderAddresses = listContributors(nickname);
+        return senderAddresses[index];
+    }
+
+    function getAmountAtIndex(string memory nickname, uint256 index)
+        public
+        view
+        returns (uint256 contributionAmount)
+    {
+        uint256[] memory contributionAmounts = listAmounts(nickname);
+        return contributionAmounts[index];
+    }
+
+    // List getters
 
     function listContributors(string memory nickname)
         public
@@ -76,5 +136,19 @@ contract Contributions is Ownable {
     {
         address recipient = getRecipientAddress(nickname);
         return _contributionAmounts[recipient];
+    }
+
+    function _deliverTokens(
+        address sender,
+        address recipient,
+        uint256 tokenAmount
+    ) internal {
+        // TODO: Debug this next
+        //          Error: VM Exception while processing transaction: revert SafeERC20: low-level call failed
+        //   at Contributions.callOptionalReturn (@openzeppelin/contracts/token/ERC20/SafeERC20.sol:68)
+        //   at Contributions.safeTransferFrom (@openzeppelin/contracts/token/ERC20/SafeERC20.sol:25)
+        //   at Contributions._deliverTokens (contracts/Contributions.sol:146)
+        // token().safeTransferFrom(sender, recipient, tokenAmount);
+        token().transferFrom(sender, recipient, tokenAmount);
     }
 }
